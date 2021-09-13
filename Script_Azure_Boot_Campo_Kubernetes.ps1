9357cbe06befd8d6651dc27e5786d97bc974d3dd
Wanderlei Grizolli Junior <jgrizolli@gmail.com>
2021-09-13 00:29:37 -0300

    Add files via upload

---
+++ Script_Azure_Boot_Campo_Kubernetes.ps1
@@ -0,0 +1,58 @@
+#Script Download Azure Kubernetes Boot Camp Channel 09
+#Criado por Wanderlei Grizolli Junior 12/09/2021
+#Me Siga no Linkedin : www.linkedin.com/in/wanderlei-g-junior
+ 
+ 
+Invoke-WebRequest	https://sec.ch9.ms/ch9/661b/9a6c6d1c-36cd-40ca-a8a6-c88aef8b661b/AKSBootcampPr%C3%A9M%C3%B3duloIntrodu%C3%A7%C3%A3odocursoeapresenta%C3%A7%C3%A3_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 1.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/efc9/1a44b3ad-4de6-491d-a1a5-af20e455efc9/AKSBootcampPr%C3%A9M%C3%B3duloObjetivosdocursoeagenda_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 2.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/efc9/1a44b3ad-4de6-491d-a1a5-af20e455efc9/AKSBootcampPr%C3%A9M%C3%B3duloObjetivosdocursoeagenda_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 3.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/c444/5178e475-5457-4db4-bd80-a39fead4c444/AKSBootcampPr%C3%A9M%C3%B3duloPr%C3%A9requisitos_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 4.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/3471/0a303db1-ddfe-46e3-89fb-c3e371633471/AKSBootcampM%C3%B3dulo0Containers_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 5.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/63d8/3164dbb0-9231-49aa-8fed-a149905863d8/AKSBootcampM%C3%B3dulo0Docker_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 6.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/6b41/6b4db431-410e-4033-b420-bbade19e6b41/AKSBootcampM%C3%B3dulo0Imagens_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 7.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/cd75/9001a84c-6b8d-4551-b60b-15b9c4e8cd75/AKSBootcampM%C3%B3dulo0Criandoumaimageminterativamente_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 8.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/e3bb/f4b03e13-81ab-483a-90cb-93fd5714e3bb/AKSBootcampM%C3%B3dulo0Criandoumaimagemdeclarativament_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 9.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/3fa2/9a1c5faa-6c24-42da-b0e3-8ecc699a3fa2/AKSBootcampM%C3%B3dulo0ContainerRegistries_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 10.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/9a43/93316595-4ef4-4edd-9916-ef0a0cad9a43/AKSBootcampM%C3%B3dulo0EnviandoimagensparaoDockerHub_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 11.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/06f4/7426a02a-fba8-4073-ab8d-4d2c1a0f06f4/AKSBootcampM%C3%B3dulo0CriandoeutilizandooACR_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 12.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/135e/443bba21-c358-41e3-abac-cfab2992135e/AKSBootcampM%C3%B3dulo1Oque%C3%A9kubernetes_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 13.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/fcd6/60afa267-8efd-42fb-a733-2b52c5eafcd6/AKSBootcampM%C3%B3dulo1CriandoumclusterAKS_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 14.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/fcd6/60afa267-8efd-42fb-a733-2b52c5eafcd6/AKSBootcampM%C3%B3dulo1CriandoumclusterAKS_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 15.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/3d8a/065a8209-6771-4c1a-a6e4-1a0f92fe3d8a/AKSBootcampM%C3%B3dulo1Oques%C3%A3oworkloads_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 16.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/b7bf/ca89fe68-5e93-41bd-9cc4-38f53e43b7bf/AKSBootcampM%C3%B3dulo1CriandonossasimagensparaoKubern_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 17.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/db4e/0872b5ef-735e-4eea-9b1e-ea50697ddb4e/AKSBootcampM%C3%B3dulo1Criandoosmanifestos_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 18.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/72eb/4d531cc4-ec2f-4ab7-92b4-aa0bcfd272eb/AKSBootcampM%C3%B3dulo1publicandoanossaaplica%C3%A7%C3%A3o_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 19.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/5209/5fcd061b-92dc-4937-9b84-516bd9135209/AKSBootcampM%C3%B3dulo2Apresenta%C3%A7aodom%C3%B3dulo_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 20.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/154d/bfdebeff-7495-40b6-8961-fe13ff7f154d/AKSBootcampM%C3%B3dulo2Conceitosearquiteturaderede_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 21.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/2826/a8c6da78-eefa-481a-8148-46c184092826/AKSBootcampM%C3%B3dulo2Services_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 22.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/7883/62e7065d-7e8f-4365-8164-d6da3bee7883/AKSBootcampM%C3%B3dulo2Ingresses_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 23.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/3caf/4e74a04f-db2d-4ab7-b870-a5b22ebb3caf/AKSBootcampM%C3%B3dulo2IngressControllers_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 24.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/45f6/662c2dae-60cb-4621-b9f6-3bc193d345f6/AKSBootcampM%C3%B3dulo2Exerc%C3%ADcioExpondooIPparaaInterne_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 25.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/e2d9/1115c53f-2ec3-47e4-b232-c6591f17e2d9/AKSBootcampM%C3%B3dulo2Exerc%C3%ADcioTestandolocalmentecomo_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 26.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/b329/b5f4d37b-5f0a-40ff-8cc9-1432101bb329/AKSBootcampM%C3%B3dulo2Exerc%C3%ADcioSubstituindoIPspordom%C3%AD_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 27.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/597a/69825243-b7c2-4442-8f14-e7955a0b597a/AKSBootcampM%C3%B3dulo3Apresenta%C3%A7%C3%A3odom%C3%B3dulo_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 28.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/7ae1/8acfdc56-c5fc-4922-8cb8-fb92b8327ae1/AKSBootcampM%C3%B3dulo3Namespaces_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 29.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/8c4a/84e22155-2bd2-4531-9252-7a42866f8c4a/AKSBootcampM%C3%B3dulo3RoleseRBAC_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 30.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/3fe0/c021066f-eb27-43a5-831b-e1387b303fe0/AKSBootcampM%C3%B3dulo3Secrets_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 31.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/0ebf/aa85f8ea-d31e-46b7-be55-d27f4a390ebf/AKSBootcampM%C3%B3dulo3Configmaps_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 32.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/edab/1a31ec90-dbfb-4371-9a0f-83d0f799edab/AKSBootcampM%C3%B3dulo3ExercicioSecrets_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 33.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/026d/10fb63bf-743d-4c96-af7e-e8e34b09026d/AKSBootcampM%C3%B3dulo3Exerc%C3%ADcioConfigmaps_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 34.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/f295/5b7be0f8-1610-463e-af5b-bf5e3cabf295/AKSBootcampM%C3%B3dulo4HelmApresenta%C3%A7%C3%A3odocap%C3%ADtulo_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 35.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/751a/1da339ae-f60c-4f09-9b5b-c3e68358751a/AKSBootcampM%C3%B3dulo4HelmOque%C3%A9Helm_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 36.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/a020/2173956c-1224-4f77-83a1-3cd40200a020/AKSBootcampM%C3%B3dulo4HelmHelmCharts_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 37.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/1282/1812336d-0152-4938-9123-8d0e97541282/AKSBootcampM%C3%B3dulo4HelmInstalandooHelm_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 38.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/ae4f/62827cfd-864f-4c37-b0e7-80ad5b7bae4f/AKSBootcampM%C3%B3dulo4HelmCriandoumchart_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 39.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/a9c7/6f2ab4be-a6ed-4462-8599-9892868ba9c7/AKSBootcampM%C3%B3dulo4HelmCriandoumarelease_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 40.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/192f/f7ce0e93-1677-48e9-8f8b-689bf77e192f/AKSBootcampM%C3%B3dulo4HelmAplicandotemplatesnobackend_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 41.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/a52b/814881df-5c10-4dbe-afd2-50fd4a2da52b/AKSBootcampM%C3%B3dulo4HelmAplicandotemplatesnofronten_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 42.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/cd47/bb77eec0-e3b7-48e3-b3a4-926505b5cd47/AKSBootcampM%C3%B3dulo4HelmCriandoumreposit%C3%B3rioHelm_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 43.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/4877/e2661dff-6bd0-4702-9d74-aecdc8994877/AKSBootcampM%C3%B3dulo5EscalabilidadeIntrodu%C3%A7%C3%A3odom%C3%B3dul_high.mp4	-OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 44.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/d244/689ccd34-a343-4d7e-8eb5-e57fee60d244/AKSBootcampM%C3%B3dulo5EscalabilidadeOque%C3%A9escalabilida_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 45.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/6a6b/78f346bd-a9f7-4c26-a548-7e0e6ea06a6b/AKSBootcampM%C3%B3dulo5EscalabilidadeEscalabilidademan_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 46.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/63e8/ad2ad093-2dde-45c8-80ce-4b152a8e63e8/AKSBootcampM%C3%B3dulo5EscalabilidadeAutoscalingintera_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 47.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/08f3/31b0ce99-35c5-4354-9253-22160ae308f3/AKSBootcampM%C3%B3dulo5EscalabilidadeAutoscalingdeclar_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 48.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/6ec2/22bbc5ae-240f-4ac4-a833-3831bbc06ec2/AKSBootcampM%C3%B3dulo5EscalabilidadeEscalabilidadeava_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 49.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/2182/64072597-bac5-46bc-b0ce-dba053372182/AKSBootcampM%C3%B3dulo5EscalabilidadeClusterautoscalin_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 50.mp4
+Invoke-WebRequest	https://sec.ch9.ms/ch9/2a70/a5525e60-92f5-426c-85e1-2284e8b02a70/AKSBootcampMateriaisRelacionadoseFinaliza%C3%A7%C3%A3odocur_high.mp4 -OutFile "COLOCAR O CAMINHO DE ONDE SERA SALVO"	\file 51.mp4
+ 
+ 
